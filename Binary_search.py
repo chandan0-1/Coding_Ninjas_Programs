69f3cbd6896e6f7891344bba51ff427ca9708b51
kingofbbsr <chandanofficial.cn@gmail.com>
2020-09-04 22:42:25 +0530

    Updated version (working for changed test cases also)

--- Binary_search.py
+++ Binary_search.py
@@ -1,18 +1,25 @@
 def Binary_Search(arr,n,target):
-	start=0
-	end=n-1
+    start=0
+    end=n-1
+    while start <= end:
+        mid = ((start+end)) //2
 
-	while start <= end:
-		mid = (start+end)//2
+        
+        if arr[mid] == target:
+            return mid
+        if arr[mid] > target:
+            end = mid-1
+        if arr[mid] < target:
+            start = mid + 1
+        # else:
+            # return mid
+    return -1
 
-		if arr[mid] > target:
-			end = mid-1
-		if arr[mid] < target:
-			start = mid + 1
-		else:
-			return mid
-	return -1
 n=int(input())
-arr = [int(x) for x in input().split()]
-target=int(input())
-print(Binary_Search(arr,n,target))
+if n>0:
+	arr = [int(x) for x in input().split()]
+else:
+    arr = []
+for _ in range(int(input())):
+    target=int(input())
+    print(Binary_Search(arr,n,target))
